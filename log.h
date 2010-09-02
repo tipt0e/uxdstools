84cdff339cadb264de3eb9e31cdf485e01c20cb2
Michael Brown <topo2@pacbell.net>
2010-09-02 02:10:16 -0700

    git remove afs.h krb5.h log.h sasl.h strings.h

--- log.h
+++
@@ -1,4 +0,0 @@
-/* log.h */
-
-/* log events */
-int log_event(char *acct, usrt type, toolop op, char *text);
--- sasl.h
+++
@@ -1,6 +0,0 @@
-/* sasl.h */
-
-/* SASL authorization handler */
-int authz_sasl_interact(LDAP * ld, unsigned flags, void *defaults,
-                        void *in);
-
