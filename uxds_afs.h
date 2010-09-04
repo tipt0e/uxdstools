bdb3e2a0eaa78d859b462da8f015449b9648cb78
Michael Brown <topo2@pacbell.net>
2010-09-03 23:01:32 -0700

    Yet Another Reorg

--- uxds_afs.h
+++
@@ -1,6 +0,0 @@
-/* afs.h */
-
-/* AFS stuff */
-typedef enum { PTSCRT, PTSGRP, PTSDEL } ptsflag;
-
-int pts_wrap(ptsflag flag, char *ptsname, char *cellname, ...);
--- uxds_sasl.h
+++
@@ -1,6 +0,0 @@
-/* sasl.h */
-
-/* SASL authorization handler */
-int uxds_sasl_interact(LDAP * ld, unsigned flags, void *defaults,
-                        void *in);
-
