9d919f7584869efeb4a36fb191e4ca966c4c3300
Michael Brown <topo2@pacbell.net>
2010-12-16 17:00:28 -0800

    eliminated unnecessary parentheses

--- src/krb5.c
+++ src/krb5.c
@@ -55,13 +55,13 @@ char *get_krbname(uxds_authz_t auth, int parse)
     char *buf;
     char *value;
 
-    if (krb5_init_context(&context) != 0) {
+    if (krb5_init_context(&context) != 0)
 	errx(1, "krb5_context");
-    }
+    
     error = krb5_cc_default(context, &id);
-    if (error) {
+    if (error)
 	krb5_err(context, 1, error, "krb5_cc_default");
-    }
+    
     if (parse == 1) {
 	value = strdup(krb5_cc_default_name(context));
 	krb5_cc_close(context, id);
@@ -70,25 +70,25 @@ char *get_krbname(uxds_authz_t auth, int parse)
 	return value;
     }
     auth.credcache = strdup(krb5_cc_default_name(context));
-    if (auth.credcache == NULL) {
+    if (auth.credcache == NULL) 
 	errx(1, "krb5_cc_default_name");
-    }
+    
     if (auth.debug)
 	fprintf(stderr,
 		"credentials cache pulled from krb5_cc_default_name: %s\n",
 		auth.credcache);
 
     error = krb5_cc_get_principal(context, id, &creds.client);
-    if (error) {
+    if (error) 
 	krb5_err(context, 1, error, "krb5_cc_get_principal");
-    }
+    
     error =
         krb5_unparse_name_flags(context, creds.client,
        			        KRB5_PRINCIPAL_UNPARSE_NO_REALM, &buf);
 
-    if (error) {
+    if (error) 
 	krb5_err(context, 1, error, "krb5_unparse_name_flags");
-    }
+    
     value = strdup(buf);
     if (auth.debug)
 	fprintf(stderr, "krb5_unparse_name_flags returned %s\n", value);
@@ -104,7 +104,7 @@ int get_tkts(char *user, char *service, uxds_authz_t auth)
 {
     krb5_context context;
     krb5_error_code error;
-    //krb5_prompt prompt;
+    /* krb5_prompt prompt; not using krb5_posix_prompter() */
     krb5_get_init_creds_opt *opt;
     krb5_creds cred;
     krb5_ccache ccache;
@@ -118,9 +118,8 @@ int get_tkts(char *user, char *service, uxds_authz_t auth)
 #if 0
 #ifdef PTS
     if (k_hasafs()) {
-	if (k_setpag() != 0) {
+	if (k_setpag() != 0) 
 	    fprintf(stderr, "Unable to create PAG\n");
-	}
     }
 #endif				/* PTS */
 #endif
@@ -131,28 +130,28 @@ int get_tkts(char *user, char *service, uxds_authz_t auth)
      */
 
     /* initialize context from krb5.conf */
-    if (krb5_init_context(&context) != 0) {
+    if (krb5_init_context(&context) != 0) 
 	errx(1, "krb5_context");
-    }
+    
     /* get defaults from krb5.conf */
     error = krb5_cc_default(context, &ccache);
-    if (error) {
+    if (error) 
 	krb5_err(context, 1, error, "krb5_cc_default");
-    }
+    
     /* set our value for below debug */
     auth.credcache = strdup(krb5_cc_default_name(context));
-    if (auth.credcache == NULL) {
+    if (auth.credcache == NULL) 
 	errx(1, "krb5_cc_default_name");
-    }
+    
     if (auth.debug)
 	fprintf(stderr,
 		"credentials cache pulled from krb5_cc_default_name: %s\n",
 		auth.credcache);
     /* set desired principal to user name from uxds_authz_t */
     error = krb5_parse_name(context, user, &target);
-    if (error) {
+    if (error) 
 	krb5_err(context, 1, error, "krb5_parse_name");
-    }
+    
     /* just checking to see if its the same */
     if (auth.debug) {
 	error =
@@ -172,9 +171,9 @@ int get_tkts(char *user, char *service, uxds_authz_t auth)
     memset(&cred, 0, sizeof(cred));
     /* we are setting non-default ticket life so we have to alloc our own creds struct */
     error = krb5_get_init_creds_opt_alloc(context, &opt);
-    if (error) {
+    if (error) 
 	krb5_err(context, 1, error, "krb5_get_init_creds_opt_alloc");
-    }
+    
     /* set "kinit" */
     krb5_get_init_creds_opt_set_default_flags(context, "kinit",
 					      krb5_principal_get_realm
@@ -199,9 +198,9 @@ int get_tkts(char *user, char *service, uxds_authz_t auth)
     /* set up auth */
     error = krb5_get_init_creds_password(context, &cred, target, NULL, NULL,	/* <- krb5_prompter_posix, */
 					 NULL, start_time, service, opt);
-    if (error) {
+    if (error) 
 	krb5_err(context, 1, error, "krb5_get_init_creds_password");
-    }
+    
     krb5_get_init_creds_opt_free(context, opt);
     /* is the password good ? */
     switch (error) {
@@ -316,23 +315,22 @@ int setpwd(char *user, char *passwd)
     krb5_data_zero(&result_code_string);
     krb5_data_zero(&result_string);
 
-    if (krb5_init_context(&context) != 0) {
+    if (krb5_init_context(&context) != 0)
 	errx(1, "krb5_context");
-    }
+    
     error = krb5_cc_default(context, &ccache);
-    if (error) {
+    if (error) 
 	krb5_err(context, 1, error, "krb5_cc_default");
-    }
-
+    
     /* it had better be /tmp/kacache_%uid or putenv() failed -->
      * fprintf(stderr,"%s is ccache\n",strdup(krb5_cc_default_name(context)));
      */
 
     error = krb5_parse_name(context, user, &target);
 
-    if (error) {
+    if (error) 
 	krb5_err(context, 1, error, "krb5_parse_name");
-    }
+    
 
     /* passwd = randstr() or mdata.setpass, depending */
     error = krb5_set_password_using_ccache(context, ccache, passwd, target,
